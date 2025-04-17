c7ce7b032dce2125eec441364cf67c96a5144197
anilaahmed1987 <anilaahmed1987@gmail.com>
2025-04-17 21:47:02 +0500

    changes done
    changes done

--- assignment-5-python-Q3/main.py
+++ assignment-5-python-Q3/main.py
@@ -1,8 +1,8 @@
-import streamlit as st
+import streamlit as st # type: ignore
 import hashlib
 import json
 import os
-from cryptography.fernet import Fernet
+from cryptography.fernet import Fernet # type: ignore
 import base64
 
 # -------------------------------------------
